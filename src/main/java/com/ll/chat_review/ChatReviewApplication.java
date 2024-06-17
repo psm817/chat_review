45576f75134fd115b9fe13886dda04c476b1d438
psm817 <5004jse@gmail.com>
2024-06-17 20:58:15 +0900

    init

---
+++ src/main/java/com/ll/chat_review/ChatReviewApplication.java
@@ -0,0 +1,13 @@
+package com.ll.chat_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/chat_review/ChatReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.chat_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
