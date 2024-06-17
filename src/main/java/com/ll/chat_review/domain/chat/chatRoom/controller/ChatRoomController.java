638fb2322b97761a4df332fca21367052eca822d
psm817 <5004jse@gmail.com>
2024-06-17 21:02:50 +0900

    chatRoomController ì 생성

---
+++ src/main/java/com/ll/chat_review/domain/chat/chatRoom/controller/ChatRoomController.java
@@ -0,0 +1,15 @@
+package com.ll.chat_review.domain.chat.chatRoom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ChatRoomController {
+    @GetMapping("/chat/room/1")
+    @ResponseBody
+    public String showRoom() {
+        return "1번 채팅방 입니다.";
+    }
+
+}
